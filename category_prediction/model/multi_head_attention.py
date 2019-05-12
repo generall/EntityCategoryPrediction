df414cabfd74946be79a0b75959cb77fd53cae7e
Andrey Vasnetsov <vasnetsov93@gmail.com>
2019-05-13 00:51:13 +0300

    multihead attention

---
+++ category_prediction/model/multi_head_attention.py
@@ -0,0 +1,123 @@
+import torch
+from allennlp.nn.util import masked_softmax, weighted_sum
+from torch.nn import Linear, MultiheadAttention, Dropout
+
+from allennlp.common import Registrable
+
+
+class MultiHeadAttention(torch.nn.Module, Registrable):
+
+    def __init__(
+            self,
+            num_heads: int,
+            attention_dropout_prob: float = 0.1
+    ):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self._attention_dropout = Dropout(attention_dropout_prob)
+
+    def forward(
+            self,
+            queries: torch.Tensor,
+            keys: torch.Tensor,
+            values: torch.Tensor,
+            mask: torch.Tensor = None
+    ):
+        """
+
+        :param queries: [batch_size x num_queries x query_size]
+        :param keys: [batch_size x seq_length x query_size]
+        :param values: [batch_size x seq_length x value_size]
+        :param mask: [batch_size x seq_length]
+
+        :return:
+            outputs: [batch_size x num_queries x value_size]
+            attention: [batch_size x num_queries x num_heads x seq_length]
+        """
+
+        num_heads = self.num_heads
+        queries_batch_size, num_queries, query_size = queries.shape
+        value_batch_size, val_seq_length, value_size = values.shape
+        keys_batch_size, keys_seq_length, keys_size = keys.shape
+
+        assert queries_batch_size == value_batch_size
+        assert keys_batch_size == value_batch_size
+        assert keys_batch_size == value_batch_size
+        assert val_seq_length == keys_seq_length
+
+        assert query_size == keys_size
+
+        seq_length = val_seq_length
+        batch_size = queries_batch_size
+
+        scale = (query_size // num_heads) ** 0.5
+
+        # Shape (num_heads * batch_size, num_queries, attention_dim / num_heads)
+        queries_per_head = queries.view(batch_size, num_queries, num_heads, query_size // num_heads)
+        queries_per_head = queries_per_head.transpose(1, 2).contiguous()
+        queries_per_head = queries_per_head.view(batch_size * num_heads, num_queries, query_size // num_heads)
+
+        if queries.data_ptr() == values.data_ptr():
+            values_per_head = queries_per_head
+        else:
+            # Shape (num_heads * batch_size, seq_length, value_size / num_heads)
+            values_per_head = values.view(batch_size, seq_length, num_heads, value_size // num_heads)
+            values_per_head = values_per_head.transpose(1, 2).contiguous()
+            values_per_head = values_per_head.view(batch_size * num_heads, seq_length, value_size // num_heads)
+
+        if values.data_ptr() == keys.data_ptr():
+            keys_per_head = values_per_head
+        else:
+            # Shape (num_heads * batch_size, seq_length, attention_dim / num_heads)
+            keys_per_head = keys.view(batch_size, seq_length, num_heads, query_size // num_heads)
+            keys_per_head = keys_per_head.transpose(1, 2).contiguous()
+            keys_per_head = keys_per_head.view(batch_size * num_heads, seq_length, query_size // num_heads)
+
+        # Shape (num_heads * batch_size, attention_dim / num_heads, seq_length)
+        keys_per_head = keys_per_head.transpose(1, 2)
+
+        # shape (num_heads * batch_size, num_queries, seq_length)
+        scaled_similarities = torch.bmm(queries_per_head / scale, keys_per_head)
+
+        # shape (num_heads * batch_size, num_queries, seq_length)
+        # Normalise the distributions, using the same mask for all heads.
+        attention = masked_softmax(scaled_similarities,
+                                   mask.repeat(1, num_heads).view(batch_size * num_heads, seq_length),
+                                   memory_efficient=False)
+
+        attention = self._attention_dropout(attention)
+
+        # Take a weighted sum of the values with respect to the attention
+        # distributions for each element in the num_heads * batch_size dimension.
+        # shape (num_heads * batch_size, num_queries, value_size/num_heads)
+        outputs = weighted_sum(values_per_head, attention)
+
+        # Reshape back to original shape (batch_size, seq_length, value_size)
+        # shape (batch_size, num_heads, num_queries, value_size/num_heads)
+        outputs = outputs.view(batch_size, num_heads, num_queries, value_size // num_heads)
+        # shape (batch_size, num_queries, num_heads, value_size/num_heads)
+        outputs = outputs.transpose(1, 2).contiguous()
+        # shape (batch_size, num_queries, value_size)
+        outputs = outputs.view(batch_size, num_queries, value_size)
+
+        attention = attention.view(batch_size, num_heads, num_queries, seq_length).transpose(1, 2)
+
+        return outputs, attention
+
+
+if __name__ == '__main__':
+    matt = MultiHeadAttention(num_heads=2)
+
+    queries = torch.rand(3, 5, 22)
+    keys = torch.rand(3, 7, 22)
+    values = torch.rand(3, 7, 26)
+    mask = torch.rand(3, 7) > 0.5
+
+    res, weights = matt.forward(queries, keys, values, mask)
+
+    print("res.shape", res.shape)
+    print("weights.shape", weights.shape)
+
+    print(mask)
+    print(weights)
+
--- category_prediction/model/seq_combiner.py
+++ category_prediction/model/seq_combiner.py
@@ -3,6 +3,9 @@ from allennlp.common import Registrable
 from allennlp.modules import Attention, FeedForward
 from allennlp.nn import Activation
 from allennlp.nn.util import clone, weighted_sum
+from torch.nn import LayerNorm
+
+from category_prediction.model.multi_head_attention import MultiHeadAttention
 
 
 class SeqCombiner(Registrable):
@@ -46,39 +49,92 @@ class LstmAttentionCombiner(torch.nn.Module, SeqCombiner):
 
     def __init__(
             self,
-            num_layers,
-            input_state_dim,
-            encoded_seq_dim,
-            output_dim,
-            dropout: float,
-            attention: Attention
+            num_layers: int,
+            input_state_dim: int,
+            encoded_seq_dim: int,
+            feed_forward_hidden_dim: int,
+            hidden_dim: int,
+            output_dim: int,
+            num_heads: int,
+            dropout_prob: float = 0.1,
+            attention_dropout_prob: float = 0.1
     ):
         super(LstmAttentionCombiner, self).__init__()
         self.num_layers = num_layers
         self.output_dim = output_dim
 
-        self.attentions = clone(attention, num_layers)
-        for att in self.attentions:
-            att.reset_parameters()
+        self._feed_forwards = []
+        self._key_mappers = []
+        self._feed_forward_layer_norm_layers = []
+        self._attentions = []
+        self._layer_norm_layers = []
+        self._lstms = []
+        self._lstm_ff = []
+        self._lstm_norm = []
 
-        self.lstm_input_dim = input_state_dim + encoded_seq_dim  # Encoded state + attention
+        self.lstm_input_dim = hidden_dim + encoded_seq_dim  # Encoded state + attention
 
-        self.lstms = torch.nn.ModuleList([
-            torch.nn.LSTM(
+        feed_forward_input_dim = input_state_dim
+        for i in range(num_layers):
+            feed_forward = FeedForward(feed_forward_input_dim,
+                                       activations=[Activation.by_name('leaky_relu')(),
+                                                    Activation.by_name('linear')()],
+                                       hidden_dims=[feed_forward_hidden_dim, hidden_dim],
+                                       num_layers=2,
+                                       dropout=dropout_prob)
+            self.add_module(f"feed_forward_layer_{i}", feed_forward)
+            self._feed_forwards.append(feed_forward)
+            feed_forward_input_dim = hidden_dim
+
+            key_mapper = FeedForward(
+                hidden_dim,
+                num_layers=1,
+                hidden_dims=encoded_seq_dim,
+                activations=Activation.by_name('linear')()
+            )
+            self._key_mappers.append(key_mapper)
+            self.add_module(f"key_mapper_layer_{i}", key_mapper)
+
+            feed_forward_layer_norm = LayerNorm(hidden_dim)
+            self.add_module(f"feed_forward_layer_norm_{i}", feed_forward_layer_norm)
+            self._feed_forward_layer_norm_layers.append(feed_forward_layer_norm)
+
+            attention = MultiHeadAttention(num_heads, attention_dropout_prob)
+            self.add_module(f"attention_layer_{i}", attention)
+            self._attentions.append(attention)
+
+            layer_norm = LayerNorm(encoded_seq_dim)
+            self.add_module(f"layer_norm_{i}", layer_norm)
+            self._layer_norm_layers.append(layer_norm)
+
+            lstm = torch.nn.LSTM(
                 input_size=self.lstm_input_dim,
-                hidden_size=int(input_state_dim / 2),
+                hidden_size=int(hidden_dim / 2),
                 batch_first=True,
                 bidirectional=True
             )
-            for _ in range(num_layers)
-        ])
+            self.add_module(f"lstm_{i}", lstm)
+            self._lstms.append(lstm)
+
+            lstm_ff = FeedForward(
+                hidden_dim,
+                num_layers=1,
+                hidden_dims=hidden_dim,
+                activations=Activation.by_name('linear')()
+            )
+            self.add_module(f"lstm_ff_{i}", lstm_ff)
+            self._lstm_ff.append(lstm_ff)
+
+            lstm_norm = LayerNorm(hidden_dim)
+            self.add_module(f"lstm_norm_{i}", lstm_norm)
+            self._lstm_norm.append(lstm_norm)
 
         self._output_projection = FeedForward(
-            input_state_dim,
+            hidden_dim,
             num_layers=1,
             hidden_dims=output_dim,
             activations=Activation.by_name('leaky_relu')(),
-            dropout=dropout
+            dropout=dropout_prob
         )
 
     def forward(
@@ -106,28 +162,54 @@ class LstmAttentionCombiner(torch.nn.Module, SeqCombiner):
         encoded_sequences_mask = encoded_sequences_mask.view(flat_size, seq_length)
 
         for i in range(self.num_layers):
-            att: Attention = self.attentions[i]
+            ff: FeedForward = self._feed_forwards[i]
+            key_mapper: FeedForward = self._key_mappers[i]
+            ff_norm: LayerNorm = self._feed_forward_layer_norm_layers[i]
+            att: Attention = self._attentions[i]
+            att_norm = self._layer_norm_layers[i]
+            lstm: torch.nn.LSTM = self._lstms[i]
 
-            lstm: torch.nn.LSTM = self.lstms[i]
+            lstm_ff: FeedForward = self._lstm_ff[i]
+            lstm_norm: LayerNorm = self._lstm_norm[i]
 
-            # shape: (flat_size * seq_length)
-            weights = att(encoded_final_states, encoded_sequences, encoded_sequences_mask)
+            # shape: (flat_size * hidden_dim)
+            cached_input = encoded_final_states
+
+            # shape: (flat_size * hidden_dim)
+            encoded_final_states = ff(encoded_final_states)
+
+            if encoded_final_states.size() == cached_input.size():
+                # shape: (flat_size * hidden_dim)
+                encoded_final_states = ff_norm(encoded_final_states + cached_input)
+
+            # shape: (flat_size * 1 * encoded_seq_dim)
+            encoded_queries = key_mapper(encoded_final_states).unsqueeze(1)
+
+            # attention_lookup shape: (flat_size * 1 * encoded_seq_dim)
+            attention_lookup, weights = att(
+                queries=encoded_queries,
+                keys=encoded_sequences,
+                values=encoded_sequences,
+                mask=encoded_sequences_mask
+            )
 
             # shape: (flat_size * encoded_seq_dim)
-            attention_lookup = weighted_sum(encoded_sequences, weights)
+            attention_lookup = att_norm(attention_lookup.squeeze(1))
 
-            # shape: (batch_size * sample_size * (input_state_dim + encoded_seq_dim))
+            # shape: (batch_size * sample_size * (hidden_dim + encoded_seq_dim))
             lstm_input = torch.cat([encoded_final_states, attention_lookup], dim=1).view(batch_size, sample_size, -1)
 
-            # lstm_processed shape: (batch_size, * seq_length * input_state_dim)
-            # lstm_hidden shape: (2 * batch_size * input_state_dim)
+            # lstm_processed shape: (batch_size, * seq_length * hidden_dim)
+            # lstm_hidden shape: (2 * batch_size * hidden_dim)
             lstm_processed, (lstm_hidden, _) = lstm(lstm_input)
 
-            # shape: (flat_size * input_state_dim)
+            # shape: (flat_size * hidden_dim)
             encoded_final_states = lstm_processed.contiguous().view(batch_size * sample_size, -1)
 
-        # shape: (batch_size * input_state_dim)
-        final_vectors = lstm_hidden.transpose(0, 1).contiguous().view(batch_size, input_state_dim)
+            if encoded_final_states.size() == cached_input.size():
+                encoded_final_states = lstm_norm(lstm_ff(encoded_final_states) + cached_input)
+
+        # shape: (batch_size * hidden_dim)
+        final_vectors = lstm_hidden.transpose(0, 1).contiguous().view(batch_size, -1)
 
         return self._output_projection(final_vectors)
-
